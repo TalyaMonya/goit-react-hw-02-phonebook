8989280a83dc0dac7da73200cea308f83835ae0f
TalyaMonya <128909188+TalyaMonya@users.noreply.github.com>
2023-10-08 23:43:06 +0300

    Done hw-2-phonebook

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import { GlobalStyle } from 'components/GlobalStyle';
 import { ThemeProvider } from 'styled-components';
+import {theme} from 'components/theme'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider theme={{}}>
+    <ThemeProvider theme={theme}>
       <App />
     <GlobalStyle />
     </ThemeProvider>
