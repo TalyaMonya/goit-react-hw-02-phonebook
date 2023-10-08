19989fde5c9edbba36244a325e072460260018ae
TalyaMonya <128909188+TalyaMonya@users.noreply.github.com>
2023-10-08 08:28:52 +0300

    Bcg-body

---
+++ src/components/GlobalStyle.js
@@ -0,0 +1,9 @@
+import { createGlobalStyle } from 'styled-components';
+import 'modern-normalize';
+
+export const GlobalStyle = createGlobalStyle`
+body {
+  min-height: 100vh;
+  background-image: url('../../public/wallpaper.jpg');
+  }
+`
